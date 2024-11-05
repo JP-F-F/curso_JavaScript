16a9c6a41eac57487d9304781a277825b89f29c5
juan <kyaraferreira31@gmail.com>
2024-11-05 13:53:51 -0300

    botando em pratica as condições

---
+++ Modulo-D/condicoes.js
@@ -0,0 +1,17 @@
+
+//exemplo de condição simples
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log('você atropelou uma velha... PARABENS')
+}
+console.log('dirija com cuidado')
+
+//---------------------------------------------------------------
+
+var pais = 'Britanico'
+if (pais == 'Brasil'){
+    console.log('Brasileiro, aeeee caba safado')
+}else{
+    console.log('gringo otario KKKKK')
+}
