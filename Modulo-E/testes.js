0328cebe47df2554b1a9f50c38de67cb103e4856
juan <kyaraferreira31@gmail.com>
2024-11-11 10:54:18 -0300

    feat: comacando o modulo-E

---
+++ Modulo-E/testes.js
@@ -0,0 +1,14 @@
+
+//usando o while em sua forma principal
+var c = 0
+while(c < 6){
+    console.log('oi')
+    c++
+}
+
+//usando o while em sua forma invertida
+
+do{
+    console.log('uepa')
+    c++
+}while(c <= 6)
